ead9ab8d0b7c1baf9e32a6badc0253c40c2647d6
zc62 <17048823+zc62@users.noreply.github.com>
2023-11-30 20:04:54 -0600

    Fix the bug that wrong unit was used

--- polarizability.py
+++ polarizability.py
@@ -4,7 +4,11 @@ from pyscf import gto, dft, grad, scf
 import numpy as np
 from pyscf.scf.hf import RHF
 from pyscf.dft.rks import RKS
-from pyscf.scf.hf import dip_moment
+from pyscf.lib import param
+
+# TODO: you can write an iterface code such that you don't
+# need to decorate every possible SCF class
+# See pyscf/qmmm/itrf.py for an example
 
 # Hartree-Fork SCF Method
 
@@ -16,18 +20,20 @@ class ModifiedRHF(RHF):
     def __init__(self, mol, e_field=np.zeros(3)):
         RHF.__init__(self, mol)
         self.e_field = e_field
-        
+
     def get_hcore(self, mol=None):
         if mol is None: mol = self.mol
         return RHF.get_hcore(self, mol) + hcore_caused_by_elec_field(mol, self.e_field)
 
 def get_dipole_with_field_RHF(mol, e_field=np.zeros(3)):
     mf = ModifiedRHF(mol, e_field=e_field)
+    mf.verbose = 0
+    mf.stdout = '/dev/null'
     mf.conv_tol_grad = 1e-8
     mf.max_cycle = 100
     mf.kernel()
     dm = mf.make_rdm1()
-    return dip_moment(mol, dm, unit='AU')
+    return mf.dip_moment(mol, dm, unit='AU', verbose=0)
 
 def get_dipole_at_grid_RHF(mol, direction, h):
     # preparation for central difference
@@ -37,25 +43,29 @@ def get_dipole_at_grid_RHF(mol, direction, h):
         elec_grid = np.array([[0, i * h, 0] for i in range(-2,3)])
     elif direction == 'z':
         elec_grid = np.array([[0, 0, i * h] for i in range(-2,3)])
-    
+
     dipole_at_grid = []
     for e_field in elec_grid:
         dipole_at_grid.append(get_dipole_with_field_RHF(mol, e_field))
-    
+
     return np.array(dipole_at_grid)
 
 def get_polarizability_RHF(mol):
+    # unit: Angstrom**3
     # 4th order central difference
     h = 0.001
     central_difference_coeff = np.array([1/12, -2/3, 0, 2/3, -1/12])/h
-    
+
     # calculate polarizability
     polarizability_caused_by_ex = np.dot(get_dipole_at_grid_RHF(mol, 'x', h).T, central_difference_coeff)
     polarizability_caused_by_ey = np.dot(get_dipole_at_grid_RHF(mol, 'y', h).T, central_difference_coeff)
     polarizability_caused_by_ez = np.dot(get_dipole_at_grid_RHF(mol, 'z', h).T, central_difference_coeff)
-    
+
     # keep the column vector format
-    return np.array([polarizability_caused_by_ex, polarizability_caused_by_ey, polarizability_caused_by_ez]).T
+    polarizability = np.array([polarizability_caused_by_ex, polarizability_caused_by_ey, polarizability_caused_by_ez]).T
+    # convert from a.u. to Angstrom**3
+    polarizability *= param.BOHR**3
+    return polarizability
 
 def get_beta_RHF(mol):
     alpha = get_polarizability_RHF(mol)
@@ -69,18 +79,20 @@ class ModifiedRKS(RKS):
     def __init__(self, mol, xc='LDA,VWN', e_field=np.zeros(3)):
         RKS.__init__(self, mol, xc=xc)
         self.e_field = e_field
-        
+
     def get_hcore(self, mol=None):
         if mol is None: mol = self.mol
         return RKS.get_hcore(self, mol) + hcore_caused_by_elec_field(mol, self.e_field)
 
 def get_dipole_with_field_DFT(mol, e_field=np.zeros(3)):
     mf = ModifiedRKS(mol, e_field=e_field, xc='b3lyp')
-    mf.conv_tol_grad = 1e-8
+    mf.verbose = 0
+    mf.stdout = '/dev/null'
+    mf.conv_tol_grad = 1e-7
     mf.max_cycle = 100
     mf.kernel()
     dm = mf.make_rdm1()
-    return dip_moment(mol, dm, unit='A.U.')
+    return mf.dip_moment(mol, dm, unit='AU', verbose=0)
 
 def get_dipole_at_grid_DFT(mol, direction, h):
     if direction == 'x':
@@ -89,25 +101,29 @@ def get_dipole_at_grid_DFT(mol, direction, h):
         elec_grid = np.array([[0, i * h, 0] for i in range(-2,3)])
     elif direction == 'z':
         elec_grid = np.array([[0, 0, i * h] for i in range(-2,3)])
-    
+
     dipole_at_grid = []
     for e_field in elec_grid:
         dipole_at_grid.append(get_dipole_with_field_DFT(mol, e_field))
-    
+
     return np.array(dipole_at_grid)
 
 def get_polarizability_DFT(mol):
+    # unit: Angstrom**3
     # 4th order central difference
     h = 0.001
     central_difference_coeff = np.array([1/12, -2/3, 0, 2/3, -1/12])/h
-    
+
     # calculate polarizability
     polarizability_caused_by_ex = np.dot(get_dipole_at_grid_DFT(mol, 'x', h).T, central_difference_coeff)
     polarizability_caused_by_ey = np.dot(get_dipole_at_grid_DFT(mol, 'y', h).T, central_difference_coeff)
     polarizability_caused_by_ez = np.dot(get_dipole_at_grid_DFT(mol, 'z', h).T, central_difference_coeff)
-    
+
     # keep the column vector format
-    return np.array([polarizability_caused_by_ex, polarizability_caused_by_ey, polarizability_caused_by_ez]).T
+    polarizability = np.array([polarizability_caused_by_ex, polarizability_caused_by_ey, polarizability_caused_by_ez]).T
+    # convert from a.u. to Angstrom**3
+    polarizability *= param.BOHR**3
+    return polarizability
 
 def get_beta_DFT(mol):
     alpha = get_polarizability_DFT(mol)
